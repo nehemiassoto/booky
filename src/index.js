eda288bf503ce4ce32640bfe7bdb1628eba94ea4
nehemias soto <nehemiassoto.epet20@gmail.com>
2022-06-29 19:41:58 -0300

    Login

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider domain="dev-x-bis1vo.us.auth0.com" clientId='Wp8z5QfEyNbXRCXaAiqQ36SDPyh7DqHc' 
+    redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
